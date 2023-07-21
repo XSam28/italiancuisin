9cc01c68f6c7ecd7310e7dbc4208f33a1eb02d07
XSam28 <petrasamuel645@gmail.com>
2023-07-21 21:11:46 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        indigo: '#ece5db',
+      },
+      fontFamily: {
+        raleway: ['Raleway']
+      }
+    },
+  },
+  plugins: [],
+}
+
